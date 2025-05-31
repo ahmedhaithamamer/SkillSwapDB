aac318776f7b508c69396600827534ac475e9466
Abdulrahman Elsisi <81362749+AbdulrahmanElsisi@users.noreply.github.com>
2025-05-31 13:48:41 +0300

    Delete skill_swap directory
    New one Uploaded

--- skill_swap/Register/auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    if(!isset($_SESSION['authenticated']))
-    {
-        $_SESSION['status'] = 'Please Login Firest';
-        header('Location: http://localhost:8888/skill_swap/register/login.php');
-        exit(0);
-    }
-?>
